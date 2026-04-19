9340d8c9f3b98b4b19e52ccc5a0efde5eaa31075
Vandana <vandanaomar65@gmail.com>
2026-04-19 19:11:09 +0530

    [Vandana] rest-security

--- middlewareandcookies/middlewareandcookies/model/member.model.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const memberSchema=new mongoose.Schema({
-    memberId:Number,
-    name:String,
-    membershipType:String
-
-});
-export default mongoose.model("Member",memberSchema);
